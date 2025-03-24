e477a73a5b2720187248d6736896265dc01aa43d
shivang21007 <shivanggupta2611@gmail.com>
2025-03-24 17:13:43 +0530

    output.tf corrected

--- output.tf
+++ output.tf
@@ -11,6 +11,6 @@ output "ec2_instance_public_ip" {
 output "ec2_public_dns" {
   value = aws_instance.my-server-01.public_dns #interpolation
 }
-output "ec2_public_dns" {
+output "ec2_private_dns" {
   value = aws_instance.my-server-01.private_dns #interpolation
 }
