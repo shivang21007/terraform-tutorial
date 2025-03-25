ba689b3c56dfa1477edb9062d5817c0aa3770c0e
shivang21007 <shivanggupta2611@gmail.com>
2025-03-25 17:39:46 +0530

    s3 backend added

--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,16 @@ terraform {
       version = "5.92.0"
     }
   }
+  # this is the backend configuration for the terraform state file to be stored in s3 bucket and lock table in dynamodb table so that it can be locked and unlocked by multiple users at the same time to avoid conflicts in the state file while running terraform apply command by multiple users at the same time on the same state file stored in s3 bucket.
+  backend "s3" {
+    bucket         = "my-tf-state-bucket-01"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "my-tf-state-lock-table-01"
+  }
 }
+
+
 
 provider "aws" {
   # Configuration options
